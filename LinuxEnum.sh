c576b19106d900e73414ecca794c8baebbd315b4
craigrachow <78630624+craigrachow@users.noreply.github.com>
2022-04-18 08:50:39 +0930

    Update LinuxEnum.sh

--- LinuxEnum.sh
+++ LinuxEnum.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-
+# to download
+# mkdir ~/nishang/ && git clone https://github.com/samratashok/nishang.git ~/nishang
 
 # OS and Kernal Info
 echo "#OS and Kernal Info"
 uname -a
+id
+whoami && hostname
+ifconfig | grep ip
+ip
+netstat
+
 
 # Bash History
 history
@@ -12,7 +19,10 @@ history
 crontab -l
 
 #file Listing
+pwd
 ls -l ~/
 ls -l /tmp
 ls -l 
+ls -l /home
+
 
